15cdb36d91fe120e6dbc85c63337d37f36bf8d6c
onTDB <github@ontdb.com>
2022-08-09 16:36:49 +0900

    Revert "Fix remote addr"
    This reverts commit b470aed66f2cb48b6449a1548877cd09d24b1e11.

--- app.py
+++ app.py
@@ -63,14 +63,6 @@ CORS(app)
 
 ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg', 'webp', 'gif']
 
-@app.before_request
-def after_request():
-    if "X-Forwarded-For" in request.headers: request.remote_addr = request.headers["X-Forwarded-For"]
-
-@app.after_request
-def after_request(response):
-    return response
-
 @app.route('/api/')
 def main():
     #return 404 not found
